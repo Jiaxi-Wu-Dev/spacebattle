93eb61e328e71531b495ebedb948c8adad0a511c
Jiaxi Wu <jiaxwu159@gmail.com>
2020-03-09 15:26:30 -0400

    ships attacking

--- app.js
+++ app.js
@@ -31,37 +31,199 @@ Ship Properties * **hull** is the same as hitpoints. If hull reaches `0` or less
 
 const random = Math.random();
 
-let ussschwarzenegger = {
-    hull: 20,
-    firepower: 5,
-    accuracy: .7,
-}
-
-/* let alienships = function(hull, firepower, accuracy){
-    this.alienhull = hull,
-    this.alienfirepower = firepower,
-    this.alienaccuracy = accuracy
-};
-
-alienships.hull = Math.random();
-alienships.firepower = Math.random();
-alienships.alienaccuracy = Math.random(); */
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-let alienship1 = {
-    hull: getRandomInt(3, 6),
-    firepower: getRandomInt(2, 4),
-    accuracy: getRandomInt(.6, .8)
+let ussschwarzenegger = {
+    hull: 20,
+    firepower: 5,
+    accuracy: .7,
 }
 
-console.log(alienship1.hull)
+let alienship1 = {
+    name: "Alien Ship One",
+    hull: getRandomInt(3, 6),
+    firepower: getRandomInt(2, 4),
+    accuracy: getRandomInt(.6, .8),
+    amount: 0
+}
 
-// console.log(getRandomInt(5, 100));
-/* console.log(alienship1.alienhull, alienship1.alienfirepower, alienship1.alienaccuracy) 
-console.log(alienship1.alienfirepower)
-console.log(alienship1.alienaccuracy) */
+let alienship2 = {
+    name: "Alien Ship Two",
+    hull: getRandomInt(3, 6),
+    firepower: getRandomInt(2, 4),
+    accuracy: getRandomInt(.6, .8),
+    amount: 0
+}
 
+let alienship3 = {
+    name: "Alien Ship Three",
+    hull: getRandomInt(3, 6),
+    firepower: getRandomInt(2, 4),
+    accuracy: getRandomInt(.6, .8),
+    amount: 0
+}
+
+let alienship4 = {
+    name: "Alien Ship Four",
+    hull: getRandomInt(3, 6),
+    firepower: getRandomInt(2, 4),
+    accuracy: getRandomInt(.6, .8),
+    amount: 0
+}
+
+let alienship5 = {
+    name: "Alien Ship Five",
+    hull: getRandomInt(3, 6),
+    firepower: getRandomInt(2, 4),
+    accuracy: getRandomInt(.6, .8),
+    amount: 0
+}
+
+let alienship6 = {
+    name: "Alien Ship Six",
+    hull: getRandomInt(3, 6),
+    firepower: getRandomInt(2, 4),
+    accuracy: getRandomInt(.6, .8),
+    amount: 00
+}
+
+var beginGame = prompt("ARE YOU READY TO DEFEND EARTH", "YES/NO");
+
+if (beginGame === "YES") {
+    document.getElementById("startGameText").innerHTML = "Lets begin the game! Right click, then click on inspect and click console";
+    console.log(" Earth has been attacked by a horde of aliens! You are the captain of the USS Schwarzenegger, on a mission to destroy every last alien ship. Battle the aliens as you try to destroy them with your lasers. There are six alien ships. Before each battle, you have the option to continue onwards to the next ship or to retreat. Beware, if you choose retreat Earth will be left defenseless and it will be game over!" )
+
+} else {
+    document.getElementById("startGameText").innerHTML = "Come back when you are! Be quick though, Earth cannot wait forever"
+}
+
+
+var getIntoShip = prompt("Time to get into the ship, ready to engage?", "YES/NO");
+
+if (getIntoShip === "YES"){
+    console.log("You are encountering your first spaceship! " + alienship1.name + " engaged!");
+    alienship1.amount = 1
+    console.log(alienship1.amount)
+    if (alienship1.amount === 1 && alienship1.hull >= 0 && ussschwarzenegger.hull >= 0){
+        console.log("preparing to fire lasers")
+        if (ussschwarzenegger.accuracy >= alienship1.accuracy){
+            console.log("you have hit the alien ship!")
+            let damageToAlienHull = alienship1.hull - 5;
+            console.log(damageToAlienHull)
+            if (alienship1.hull >= 0) {
+                console.log(alienship1.name + " destroyed");
+                alienship1.amount = 0
+                let fightSecondShip = prompt("Ready to fight the Second Alien Ship?", "YES/NO")
+                if (fightSecondShip === "YES") {
+                    console.log("Attacking Second Ship")
+                    alienship2.amount = 1
+                    if (alienship2.amount === 1 && alienship2.hull >= 0 && ussschwarzenegger.hull >= 0) {
+                        console.log("preparing to fire lasers")
+                        if (ussschwarzenegger.accuracy >= alienship2.accuracy){
+                            console.log("you have hit the alien ship!")
+                            let damageToAlienHull2 = alienship2.hull - 5;
+                            console.log(damageToAlienHull2)
+                            if (alienship2.hull >= 0) {
+                                console.log(alienship2.name + " destroyed");
+                                alienship2.amount = 0
+                                let fightThirdShip = prompt("Ready to fight the Third Alien Ship?", "YES/NO")
+                                if (fightThirdShip === "YES"){
+                                    console.log("Attacking Third Ship")
+                                    alienship3.amount = 1
+                                    if (alienship3.amount = 1 && alienship3.hull >= 0 && ussschwarzenegger.hull >= 0)
+                                        console.log("preparing to fire lasers")
+                                        if (ussschwarzenegger.accuracy >= alienship3.accuracy){
+                                            console.log("you have hit the alien ship!")
+                                            let damageToAlienHull3 = alienship3.hull - 5;
+                                            console.log(damageToAlienHull3)
+                                            if (alienship3.hull >= 0) {
+                                                console.log(alienship3.name + " destroyed");
+                                                alienship3.amount = 0
+                                                let fightFourthShip = prompt("Ready to fight the Fourth Alien Ship?", "YES/NO")
+                                                if (fightFourthShip = "YES") { 
+                                                    console.log("Attacking Fourth Ship")
+                                                    alienship4.amount = 1
+                                                    if (alienship4.amount = 1 && alienship4.hull >= 0 && ussschwarzenegger.hull >= 0)
+                                                    console.log("preparing to fire lasers")
+                                                        if (ussschwarzenegger.accuracy >= alienship4.accuracy){
+                                                            console.log("you have hit the alien ship!")
+                                                            let damageToAlienHull4 = alienship4.hull - 5;
+                                                            console.log(damageToAlienHull4)
+                                                            if (alienship4.hull >= 0) {
+                                                                console.log(alienship4.name + " destroyed")
+                                                                alienship4.amount = 0
+                                                                let fightFifthShip = prompt("Ready to fight the Fifth Alien Ship?", "YES/NO")
+                                                                if (fightFifthShip === "YES"){
+                                                                    console.log("Attacking Fifth Ship")
+                                                                    alienship5.amount = 1
+                                                                    if (alienship5.amount = 1 && alienship5.hull >= 0 && ussschwarzenegger.hull >= 0){
+                                                                        console.log("preparing to fire lasers")
+                                                                        if (ussschwarzenegger.accuracy >= alienship5.accuracy) {
+                                                                            console.log("you have hit the alien ship!")
+                                                                            let damageToAlienHull5 = alienship5.hull - 5;
+                                                                            console.log(damageToAlienHull5)
+                                                                            if (alienship5.hull >= 0) {
+                                                                                console.log(alienship5.name + " destroyed")
+                                                                                alienship5.amount = 0
+                                                                                let fightSixthShip = prompt("Ready to fight the Sixth Alien Ship?", "YES/NO") 
+                                                                                if (fightSixthShip = "YES"){
+                                                                                    console.log("Attacking sixth Ship")
+                                                                                    alienship6.amount = 1
+                                                                                    if (alienship6.amount = 1 && alienship6.hull >= 0 && ussschwarzenegger.hull >= 0){
+                                                                                    console.log("preparing to fire lasers")
+                                                                                    if (ussschwarzenegger.accuracy >= alienship6.accuracy) {
+                                                                                    console.log("you have hit the alien ship!")
+                                                                                    let damageToAlienHull6 = alienship6.hull - 5;
+                                                                                    console.log(damageToAlienHull6)
+                                                                                    if (alienship6.hull >= 0) {
+                                                                                    console.log(alienship6.name + " destroyed, You have successfully defended Earth!")
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    } else {
+                                                                        console.log("GAME OVER")
+                                                                    }
+                                                                } else {
+                                                                    console.log("GAME OVER")
+                                                                }
+                                                
+                                                            }
+                                                        }
+                                                } else {
+                                                    console.log("GAME OVER")
+                                                }
+                                               
+                    }
+                } else {
+                    console.log("Game Over")
+                }
+            } else {
+                console.log("GAME OVER")
+            }
+        } else {
+            console.log("You missed!")
+            let damageToUssHull = ussschwarzenegger.hull - alienship1.firepower
+            console.log(damageToUssHull)
+        }
+
+} else {
+    console.log("Who will defend Earth?")
+} 
+        }else {
+            console.log("Game Over")
+        }
+    }
+}
+
+    }
+} else {
+    console.log("Game Over")
+}
+}
+        }
+    }
+}
---
+++ comments.js
@@ -0,0 +1,43 @@
+/* let alienships = function(hull, firepower, accuracy){
+    this.alienhull = hull,
+    this.alienfirepower = firepower,
+    this.alienaccuracy = accuracy
+};
+
+alienships.hull = Math.random();
+alienships.firepower = Math.random();
+alienships.alienaccuracy = Math.random(); */
+
+
+// console.log(getRandomInt(5, 100));
+/* console.log(alienship1.alienhull, alienship1.alienfirepower, alienship1.alienaccuracy) 
+console.log(alienship1.alienfirepower)
+console.log(alienship1.alienaccuracy) */
+/* 
+alienship1.amount + 1 
+if (alienship1.amount = 1 && alienship1.hull >= 0 && ussschwarzenegger.hull >= 0){
+    console.log("preparing to fire lasers")
+    if (ussschwarzenegger.accuracy <= alienship1.accuracy){
+        console.log("you have hit the alien ship!")
+        alienship.hull - 5;
+        console.log(alienship.hull)
+    } else {
+        console.log("You missed!")
+        ussschwarzenegger.hull - alienship1.firepower
+        console.log(ussschwarzenegger.hull)
+    }
+} else if (ussschwarzenegger >= alienship1.accuracy) {
+
+}
+ */
+
+/*while (alienship1 === 1 && ussschwarzenegger.hull >= 0){
+    if (ussschwarzenegger.accuracy <= alienship1.accuracy) {
+        console.log(ussschwarzenegger.accuracy)
+        let damageToAlienShip = alienship1.hull - ussschwarzenegger.firepower;
+        console.log(damageToAlienShip)
+    } else {
+        let damageToUss = ussschwarzenegger.hull - alienship1.firepower 
+        console.log(damageToUss + " Ships been hit!")
+    }
+} */
